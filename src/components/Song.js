7ea47c506f1dec9c1ec5d628d9d609b6df0f8a2c
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-10-30 20:26:03 +0000

    Add audio preview to songs

--- src/components/Song.js
+++ src/components/Song.js
@@ -6,11 +6,16 @@ const Song = (props) => {
     const title = props.song['im:name'].label;
     const artist = props.song['im:artist'].label;
     const genre = props.song.category.attributes.label;
+    const audio = props.song.link[1].attributes.href;
+    console.log(audio);
     
 
     return(
         <div className='song'>
             <img src={image} alt={`${title} from ${artist}`} width='170px' height='170px' />
+            <audio controls className='audio'>
+                <source src={audio} type = "audio/x-m4a"/>
+            </audio>
             <h4>#{props.position} {title}</h4>
             <h5>Artist: {artist}</h5>
             <h5>Genre: {genre}</h5>
