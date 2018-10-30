615d64fbbb6924d9fdde3ab4a6dca9198fd47c2c
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-10-30 17:12:37 +0000

    Add constructor and request to SongBox

--- src/containers/SongBox.js
+++ src/containers/SongBox.js
@@ -4,6 +4,25 @@ import SongList from '../components/SongList.js'
 
 export default class SongBox extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            songs: [],
+            yehhaaa: 'is working',
+            currentSong: null
+        }
+    }
+
+    componentDidMount() {
+
+        fetch('https://itunes.apple.com/gb/rss/topsongs/limit=20/json') 
+            .then((response) => response.json())
+            .then((data) => this.setState({
+                songs: data.feed.entry
+            }));
+        console.log(this.state.songs);
+    }
+
     render() {
         return(
             <SongList />
