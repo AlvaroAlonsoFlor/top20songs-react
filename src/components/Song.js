291ed061e933071b56e5b3dec62f5384df648dc6
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-10-30 19:45:01 +0000

    Add some css to the songs and list

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import './song.css';
 
 const Song = (props) => {
     const image = props.song['im:image'][2].label;
-    console.log(image);
+    const title = props.song['im:name'].label;
+    const artist = props.song['im:artist'].label;
+    const genre = props.song.category.attributes.label;
+    
 
     return(
         <div className='song'>
-            <img src={image} alt='' width='170px' height='170px' />
+            <img src={image} alt={`${title} from ${artist}`} width='170px' height='170px' />
+            <h4>#{props.position} {title}</h4>
+            <h5>Artist: {artist}</h5>
+            <h5>Genre: {genre}</h5>
         </div>
     );
 }
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Song from './Song.js';
+import './song_list.css'
 
 const SongList = (props) => {
-    const songList = props.songs.map((song) => {
-        return <Song song={song} key={song.id.attributes['im:id']}/>
+    const songList = props.songs.map((song, position) => {
+        return <Song song={song} key={song.id.attributes['im:id']} position={position + 1}/>
     })
 
     return (
